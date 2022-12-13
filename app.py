bc96df35f38b64449cf621e9174fe943e85e042c
robertskibinski <109416561+robertskibinski@users.noreply.github.com>
2022-12-13 14:25:30 +0100

    Adding folders

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, redirect, render_template
+app = Flask(__name__)
+
+
+@app.route('/me')
+def my():
+    return render_template('name.html')
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    return render_template('contact.html')
+    if request.method == 'GET':
+        print("We received GET")
+        return render_template("form.html")
+    elif request.method == 'POST':
+        print("We received POST")
+        print(request.form)
+        return redirect("/")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
